9be2751055d929d5d13815e26745e6854db8b670
Jaima-Wasir-Shashi <jaimashasi222@gmail.com>
2023-12-23 12:23:41 +0600

    html to react converted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import AddEntry from "./components/AddEntry";
+import Listing from "./components/Listing";
 
 function App() {
   return (
     <div>
       <Header />
       <AddEntry />
-      {/* <Listing /> */}
+      <Listing />
     </div>
   );
 }
